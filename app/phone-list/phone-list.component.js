bcc516a1100cc1fb0e6d657560eac37a3d3f5288
turquoise-orchid <turquoise.orchid@gmail.com>
2017-08-14 22:13:58 +0300

    step-6 Two-way Data Binding

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -6,17 +6,21 @@ angular
 			this.phones = [
 				{
 				name: "Nexus S",
-				description: "Fast just got faster with Nexus S."
+				description: "Fast just got faster with Nexus S.",
+				age: 1
 				},
 				{
 				name: "Motorola XOOM™ with Wi-Fi",
-				description: "The Next, Next Generation tablet."
+				description: "The Next, Next Generation tablet.",
+				age: 2
 				},
 				{
 				name: "MOTOROLA XOOM™",
-				description: "The Next, Next Generation tablet"
+				description: "The Next, Next Generation tablet",
+				age: 3
 				}
 			];
+			this.orderProp = 'age';
 		}
 	}
 );
