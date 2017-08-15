bbdebb850f648fea2dd955ed0c4c940898e73372
turquoise-orchid <turquoise.orchid@gmail.com>
2017-08-15 14:04:55 +0300

    step-7 XHR & Dependency Injection

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,25 +2,13 @@ angular
 	.module("phoneList")
 	.component("phoneList", {
 		templateUrl: 'phone-list/phone-list.template.html',
-		controller: function PhoneListCtrl() {
-			this.phones = [
-				{
-				name: "Nexus S",
-				description: "Fast just got faster with Nexus S.",
-				age: 1
-				},
-				{
-				name: "Motorola XOOM™ with Wi-Fi",
-				description: "The Next, Next Generation tablet.",
-				age: 2
-				},
-				{
-				name: "MOTOROLA XOOM™",
-				description: "The Next, Next Generation tablet",
-				age: 3
-				}
-			];
-			this.orderProp = 'age';
-		}
-	}
-);
+		controller: [ "$http",
+			function PhoneListCtrl($http) {
+				var self = this;
+				self.orderProp = 'age';
+				$http.get('phones/phones.json').then(function(respond) {
+					self.phones = respond.data;
+				});
+			}
+		]	
+	});
